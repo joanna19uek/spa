b0b2f3aaae261c2abf37cec47a5032a906bded8b
s-116-27 <s-116-27@gumiforest.ae.krakow.pl>
2017-04-25 18:06:09 +0200

    08

--- 08-SinglePageApp/view.js
+++ 08-SinglePageApp/view.js
@@ -1,8 +1,10 @@
 var view = {
 		display: function (data) {
 			// create new data view to be displayed
-			var elem, text, i;
+			var cell, text, i, table, header, htext, row;
+            var headers = ["Code", "Name", "Rate"];
 			this.clear();
+            /*
 			for (i = 0; i < data.length; i++) {
 				elem = document.createElement("h3");
 				text = document.createTextNode(
@@ -10,6 +12,34 @@ var view = {
 						);
 				elem.appendChild(text);
 				document.getElementById('dataview').appendChild(elem);
+			}
+            */
+            table = document.createElement("table");
+            document.getElementById('dataview').appendChild(table);
+            header = document.createElement("hr");
+            document.getElementByTagName("table")[0].appendChild(header);
+            for (i = 0; i < 3; i++) {
+                cell = document.createElement("td");
+                htext = document.createTextNode(headers[i]);
+                cell.appendChild(htext);
+                document.getElementByTagName("hr")[0].appendChild(cell);
+            }
+            //header = document.createElement("hr");
+            for (i = 0; i < data.length; i++) {
+				row = document.createElement("tr");
+                document.getElementByTagName("table")[0].appendChild(row);
+                cell = document.createElement("td");
+                text = document.createTextNode(data[i].code);
+                cell.appendChild(text);
+                document.getElementByTagName("tr")[i].appendChild(cell);
+                
+                //cell = document.createElement("td");
+                
+				//text = document.createTextNode(
+				//		data[i].code + "/" + data[i].name + " " + data[i].rate
+				//		);
+				//elem.appendChild(text);
+				//document.getElementById('dataview').appendChild(elem);
 			}
 		},
 		clear: function () {
