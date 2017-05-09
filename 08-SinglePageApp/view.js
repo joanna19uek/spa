76415e30b21ce671c385b407aee10e614a5c9257
Joanna Pełeńska <Joanna Peeska>
2017-05-09 15:27:26 +0200

    08 c.d.

--- 08-SinglePageApp/view.js
+++ 08-SinglePageApp/view.js
@@ -15,23 +15,31 @@ var view = {
 			}
             */
             table = document.createElement("table");
+            table.setAttribute("border", "1");
+            //table.style.borderCollapse = "collapse";
+            
             document.getElementById('dataview').appendChild(table);
+            
+            var parentTable = document.getElementsByTagName("table")[0];
+            
             header = document.createElement("hr");
-            document.getElementByTagName("table")[0].appendChild(header);
+            parentTable.appendChild(header);
+            
             for (i = 0; i < 3; i++) {
                 cell = document.createElement("td");
                 htext = document.createTextNode(headers[i]);
                 cell.appendChild(htext);
-                document.getElementByTagName("hr")[0].appendChild(cell);
+                document.getElementsByTagName("hr")[0].appendChild(cell);
             }
+            
             //header = document.createElement("hr");
             for (i = 0; i < data.length; i++) {
 				row = document.createElement("tr");
-                document.getElementByTagName("table")[0].appendChild(row);
+                parentTable.appendChild(row);
                 cell = document.createElement("td");
                 text = document.createTextNode(data[i].code);
                 cell.appendChild(text);
-                document.getElementByTagName("tr")[i].appendChild(cell);
+                document.getElementsByTagName("tr")[i].appendChild(cell);
                 
                 //cell = document.createElement("td");
                 
